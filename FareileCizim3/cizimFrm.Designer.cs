9213d1487e975943445eee94b6c9417f42ca7479
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2024-01-04 15:41:40 +0300

    Proje dosyası ekle.

---
+++ FareileCizim3/cizimFrm.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace FareileCizim3
+{
+    partial class cizimFrm
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // cizimFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1081, 606);
+            this.Name = "cizimFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Çizim";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.cizimFrm_MouseDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ FareileCizim3/cizimFrm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FareileCizim3
+{
+    public partial class cizimFrm : Form
+    {
+        Graphics g;
+        List<Color> colors;
+        public cizimFrm()
+        {
+            InitializeComponent();
+            g=this.CreateGraphics();
+            colors = new List<Color>();
+            colors.Add(Color.Blue);
+            colors.Add(Color.Red);
+            colors.Add(Color.Black);
+            colors.Add(Color.Yellow);
+            colors.Add(Color.Orange);
+            colors.Add(Color.Purple);
+        }
+
+        public void cizgiCiz(Pen pen) 
+        {
+            Random r = new Random();
+            int x1 = r.Next(this.Width);
+            int y1= r.Next(this.Height);
+            int x2 = r.Next(this.Width);
+            int y2 = r.Next(this.Height);
+            g.DrawLine(pen,x1, y1, x2, y2);
+        }
+
+        public void dortgenCiz(Pen pen)
+        {
+            Random r = new Random();
+            int x1 = r.Next(this.Width);
+            int y1 = r.Next(this.Height);
+            int x2 = r.Next(this.Width);
+            int y2 = r.Next(this.Height);
+            g.DrawRectangle(pen,x1,y1,x2,y2);
+        }
+        public void elipsCiz(Pen pen)
+        {
+            Random r = new Random();
+            int x1 = r.Next(this.Width);
+            int y1 = r.Next(this.Height);
+            int x2 = r.Next(this.Width);
+            int y2 = r.Next(this.Height);
+            g.DrawEllipse(pen, x1, y1, x2, y2);
+        }
+
+        private void cizimFrm_MouseDown(object sender, MouseEventArgs e)
+        {
+            Random r = new Random();
+          for (int i = 0;i<100;i++)
+            {
+                Pen p = new Pen(colors[r.Next(colors.Count)], 3);
+                int x = r.Next(3);
+                if(x==0)
+                {
+                    cizgiCiz(p);
+                }
+                if (x == 1)
+                {
+                    dortgenCiz(p);
+                }
+                if (x == 2)
+                {
+                    elipsCiz(p);
+                }
+            }
+        }
+    }
+}
